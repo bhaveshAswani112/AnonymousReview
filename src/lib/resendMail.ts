264dad75e3fb891c38e415fe9dbd01f2e5168d29
bhaveshAswani112 <aswanib133@gmail.com>
2024-08-17 20:21:21 +0530

    build error 3 fixed

--- src/lib/resendMail.ts
+++ src/lib/resendMail.ts
@@ -3,5 +3,5 @@ import { Resend } from 'resend';
 
 // console.log("I am resend api key " + process.env.RESEND_API_KEY)
 
-const api_key : string = process.env.RESEND_API_KEY || ""
+const api_key : string = process.env.RESEND_API_KEY || "re_123"
 export const resend = new Resend(api_key);
