1b2b642e1276396d2b67492794d1880d2155b086
bhaveshAswani112 <aswanib133@gmail.com>
2024-08-17 20:18:49 +0530

    build error 2 fixed

--- src/lib/resendMail.ts
+++ src/lib/resendMail.ts
@@ -2,4 +2,6 @@ import { Resend } from 'resend';
 
 
 // console.log("I am resend api key " + process.env.RESEND_API_KEY)
-export const resend = new Resend(process.env.RESEND_API_KEY);
+
+const api_key : string = process.env.RESEND_API_KEY || ""
+export const resend = new Resend(api_key);
